e439b1d3cdf928f900e43579c1c6e68f762dd0e6
gsepdev <ggaby_sepulveda@hotmail.com>
2021-09-06 21:15:15 +1000

    change seeting 2

--- ft_blog/settings.py
+++ ft_blog/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
