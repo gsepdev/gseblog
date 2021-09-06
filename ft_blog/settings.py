5df7ef8feab6cb986da35ab597fb8d273ebf99d1
gsepdev <ggaby_sepulveda@hotmail.com>
2021-09-06 23:16:22 +1000

    staticfiles commit

--- ft_blog/settings.py
+++ ft_blog/settings.py
@@ -133,14 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
